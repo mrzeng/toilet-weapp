95bcfc884155bc77164f03103a3d19daa3f9ff49
liumingmusic <liuming_music@163.com>
2017-04-16 21:13:26 +0800

    完成找厕所主要功能(搜索附近测试，定位导航)，其中导航需要手机安装相应的地图app

---
+++ pages/index/index.js
@@ -0,0 +1,123 @@
+// 引入SDK核心类
+var QQMapWX = require('../../resources/map/qqmap-wx-jssdk.js');
+var qqmapsdk;
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    list: [],
+    scrollTop: 0,
+    size: 0,
+    onLine: true
+  },
+  // 页面加载
+  onLoad: function () {
+    wx.showLoading({ title: "获取数据中,别急!" });
+  },
+  // 页面显示
+  onShow() {
+    this.getData();
+  },
+  //获取数据
+  getData: function () {
+    var that = this;
+    // 实例化API核心类
+    qqmapsdk = new QQMapWX({
+      key: '2EQBZ-3XM36-RUTSG-MIO6B-GXH4E-B3FC5'
+    });
+    //查询附件的资源信息
+    wx.getLocation({
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        //源码里面查询的是附近一公里的哦
+        qqmapsdk.search({
+          keyword: '厕所',
+          location: {
+            latitude: latitude,
+            longitude: longitude
+          },
+          success: function (res) {
+            //根据返回的结果marker在地图上面
+            var data = res.data;
+            that.setList(data);
+            //关闭loading
+            wx.hideLoading();
+          },
+          fail: function () {
+            //关闭loading
+            wx.hideLoading();
+            //有可能是参数有问题或者是网络
+            that.setData({
+              onLine: false
+            });
+          }
+        });
+      }
+    });
+  },
+  //组装数据信息
+  setList: function (data) {
+    var that = this;
+    var result = [];
+    //循环遍历数据， 其实不做这一步也行
+    data.forEach(function (item, index) {
+      //替换一些不必要的大信息
+      var reg = new RegExp(item.ad_info.province + item.ad_info.city + item.ad_info.district);
+      var briefAddr = item.address.replace(reg, "");
+      //组装数据
+      result.push({
+        distance: item["_distance"],
+        briefAddr: briefAddr,
+        address: item.address,
+        category: item.category,
+        id: item.id,
+        latitude: item.location.lat,
+        longitude: item.location.lng,
+        name: item.title
+      });
+    });
+    //设置data
+    that.setData({
+      list: result,
+      size: result.length
+    });
+  },
+  //点击列表显示本地导航信息
+  tapItem: function (e) {
+    var id = e.currentTarget.dataset.id;
+    var that = this;
+    var toilet = that.findMarkerById(id);
+    //使用微信内置地图查看位置
+    wx.openLocation({
+      latitude: toilet.latitude,
+      longitude: toilet.longitude,
+      name: toilet.name,
+      address: toilet.address,
+      scale: 28
+    });
+  },
+  //根据marker唯一id查询信息
+  findMarkerById: function (id) {
+    var that = this,
+      result = {};
+    var list = that.data.list;
+    //查询数据信息
+    for (var i = 0; i < list.length; i++) {
+      if (id === list[i].id) {
+        result = list[i];
+        break;
+      }
+    }
+    return result;
+  },
+  // 数据更新
+  doRefresh: function () {
+    wx.showLoading({ title: "数据更新中,别急!" });
+    this.getData();
+  },
+  // 设置界面跳转 跳转到关于界面
+  navToSetting: function () {
+    
+  }
+})
---
+++ pages/setting/setting.js
@@ -0,0 +1,15 @@
+//获取应用实例
+var app = getApp()
+Page({
+    // 数据
+    data: {
+
+    },
+    // 页面加载
+    onLoad: function () {
+    },
+    // 页面显示
+    onShow() {
+
+    }
+})
